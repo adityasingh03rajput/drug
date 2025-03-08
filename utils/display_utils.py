bb09c7e541a30f1daa7e6386068fc09ba73a753a
adityasingh03rajput <123081746+adityasingh03rajput@users.noreply.github.com>
2025-03-08 16:39:18 +0530

    Update display_utils.py

--- utils/display_utils.py
+++ utils/display_utils.py
@@ -1,1 +1,8 @@
+import streamlit as st
 
+def display_results(compound):
+    if compound:
+        st.success(f"✅ Identified as {compound.iupac_name}.")
+        st.write(f"Molecular Formula: {compound.molecular_formula}")
+    else:
+        st.error("❌ Not a drug. Likely Water or another substance.")
