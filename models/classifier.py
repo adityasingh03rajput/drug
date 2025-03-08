04ee92e0a5b4f9da1ba60dea7ec8978b676e804b
adityasingh03rajput <123081746+adityasingh03rajput@users.noreply.github.com>
2025-03-08 16:38:02 +0530

    Update classifier.py

--- models/classifier.py
+++ models/classifier.py
@@ -1,1 +1,4 @@
-
+def classify_substance(compound):
+    if compound:
+        return f"✅ Identified as {compound.iupac_name}. Molecular Formula: {compound.molecular_formula}"
+    return "❌ Not a drug. Likely Water or another substance."
