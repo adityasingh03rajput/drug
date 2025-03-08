ef3440d33b61102c35dd149c7a418f29a1e7ae9e
adityasingh03rajput <123081746+adityasingh03rajput@users.noreply.github.com>
2025-03-08 16:37:32 +0530

    Update feature_extractor.py

--- models/feature_extractor.py
+++ models/feature_extractor.py
@@ -1,1 +1,8 @@
+import tensorflow as tf
+import numpy as np
 
+def extract_features(image):
+    model = tf.keras.applications.EfficientNetB0(weights="imagenet", include_top=False, pooling="avg")
+    image = np.expand_dims(image, axis=0)  # Add batch dimension
+    features = model.predict(image)
+    return features.flatten()
