3b66923066cfbcbbca2980b87eab4cfb79133cfb
adityasingh03rajput <123081746+adityasingh03rajput@users.noreply.github.com>
2025-03-08 16:39:00 +0530

    Update image_utils.py

--- utils/image_utils.py
+++ utils/image_utils.py
@@ -1,1 +1,8 @@
+from PIL import Image
+import numpy as np
 
+def preprocess_image(file):
+    image = Image.open(file).convert("RGB")
+    image = image.resize((224, 224))  # Resize for model input
+    image = np.array(image) / 255.0  # Normalize
+    return image
