0194cb0e90d967592938d85563d310e7ff44cfe6
adityasingh03rajput <123081746+adityasingh03rajput@users.noreply.github.com>
2025-03-08 17:00:01 +0530

    Update pubchem_client.py

--- database/pubchem_client.py
+++ database/pubchem_client.py
@@ -1,5 +1,4 @@
 import pubchempy as pcp
-import time
 
 def query_pubchem(features):
     try:
@@ -8,14 +7,10 @@ def query_pubchem(features):
         
         # Query PubChem
         compounds = pcp.get_compounds(query, namespace="smiles", limit=1)
-        time.sleep(1)  # Add a delay to avoid rate limits
         
         if compounds:
             return compounds[0]  # Return the first match
         return None
-    except pcp.BadRequestError as e:
-        print(f"PubChem API Error: {e}")
-        return None
     except Exception as e:
-        print(f"Unexpected Error: {e}")
+        print(f"Error querying PubChem: {e}")
         return None
