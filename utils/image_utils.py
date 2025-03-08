57e1d1cbc8881d4324ed91a6f3fe4d2a81e59969
adityasingh03rajput <123081746+adityasingh03rajput@users.noreply.github.com>
2025-03-08 17:00:28 +0530

    Update image_utils.py

--- utils/image_utils.py
+++ utils/image_utils.py
@@ -2,7 +2,8 @@ from PIL import Image
 import numpy as np
 
 def preprocess_image(file):
+    # Open and resize the image
     image = Image.open(file).convert("RGB")
-    image = image.resize((224, 224))  # Resize for model input
-    image = np.array(image) / 255.0  # Normalize
+    image = image.resize((224, 224))  # Resize for EfficientNet input
+    image = np.array(image)  # Convert to numpy array
     return image
